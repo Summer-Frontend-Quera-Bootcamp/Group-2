5a837cf82166fd1bfc6ec47ac0d22076d141df81
eli-tay <elmira.tayefeh@gmial.com>
2023-09-21 03:51:39 +0330

    Tailwind.config.js has been set. create login page CMP

---
+++ src/components/LoginRegister/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../Header"
+import "./style.css";
+export const LoginRegister = () =>{
+return(
+    <div className="backColor h-screen">
+     <div className="container mx-auto relative h-screen ">
+        <Header/>
+     </div> 
+     </div>
+)
+}
+
+//export default LoginRegister;
