9c408e351b14fb7c426c18ee02247f81db4f0eea
eli-tay <elmira.tayefeh@gmial.com>
2023-09-21 17:35:44 +0330

    create login component's

---
+++ src/components/LoginRegister/Login/index.tsx
@@ -0,0 +1,46 @@
+interface IFormLoginProps {
+   TitleBox:string;
+};
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../Button";
+
+import Input from "../Input";
+
+import "./style.css";
+ const Login:React.FC<IFormLoginProps> = ({TitleBox}):JSX.Element  =>{
+ 
+   return(
+      <>
+      <h1 className="font-bold text-center">{TitleBox}</h1>
+      <form>
+      {/* Email input  */}
+      <Input   LabelName="ایمیل" LabelFor="UserEmail" InputType="text"/>
+
+      {/* Password input */}
+      <Input   LabelName="رمز عبور" LabelFor="UserPass" InputType="password"/>
+
+      {/* Forgot password link */}
+      <div className="mb-6 flex items-center justify-between">            
+         <Link to="" title="فراموشی رمز عبور" className="text-green-primary">رمزعبور را فراموش کرده ای ؟</Link>
+      </div>
+
+          {/* Login button  */}
+          <div className="text-center lg:text-left">
+            <Button  DspWidth="w-full" BtnValue="ورود" BtnColor="bg-green-primary" />
+
+            {/* Register link */}
+            <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
+              ثبت نام نکرده ای؟
+              <a
+                href="#!"
+                className="text-green-primary transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700 ps-3"
+                >ثبت نام</a
+              >
+            </p>
+          </div>
+        </form>
+      </>
+) }
+
+export default Login;
--- src/components/LoginRegister/index.tsx
+++ src/components/LoginRegister/index.tsx
@@ -1,14 +1,22 @@
 import React from "react"
 import Header from "../Header"
+import Login from "./Login";
 import "./style.css";
-export const LoginRegister = () =>{
+const LoginRegister:React.FC = () :JSX.Element =>{
 return(
     <div className="backColor h-screen">
      <div className="container mx-auto relative h-screen ">
         <Header/>
+        <div className="row justify-center">
+            <div className="lg:w-[50%] md:w-[80%] card  md:mt-50 mt-10">
+            <Login TitleBox="به کوئرا تسک منیجر خوش برگشتی :)"/>
+            </div>
+         </div>
      </div> 
+
+     
      </div>
 )
 }
 
-//export default LoginRegister;
+export default LoginRegister;
