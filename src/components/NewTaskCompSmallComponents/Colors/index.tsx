b06231f746fcce6fb68456c1309ac5b36af93cd6
alireza <adver501@chmail.ir>
2023-10-14 14:22:59 +0330

    Colors component created!

---
+++ src/components/NewTaskCompSmallComponents/Colors/index.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import "./style.css";
+const Colors: React.FC = (): JSX.Element => {
+    return (
+        <div className="container">
+            <div className="colorsContainer">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#FD7E14" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#FAB005" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#82C91E" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#40C057" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#208D8E" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#12B886" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#15AABF" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#228BE6" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#4C6EF5" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#7950F2" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#BE4BDB" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#BE4BDB" />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <rect width="20" height="20" rx="8" fill="#FA5252" />
+                </svg>
+            </div>
+        </div>
+    );
+};
+
+export default Colors;
