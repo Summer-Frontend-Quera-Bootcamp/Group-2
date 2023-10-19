9fa6fc1608f0db9fd16942b5b1207df8f7067397
eli-tay <elmira.tayefeh@gmail.com>
2023-10-19 22:13:17 +0330

    Update AlignRight.tsx

--- src/components/ShareComponent/Icons/AlignRight.tsx
+++ src/components/ShareComponent/Icons/AlignRight.tsx
@@ -12,7 +12,7 @@ export default function AlignRight({
       height={size} // added size here
       fill={color} // added color here
     >
-      <path d="M8 10H21M3 14H21M8 18H21M3 6H21" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M8 10H21M3 14H21M8 18H21M3 6H21" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
  </svg>
   )
 }
